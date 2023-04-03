a13cb978ea4c8f2bac30b9baeab9e61b37c8327d
tothv1 <50296138+vitya0717@users.noreply.github.com>
2023-04-03 11:24:32 +0200

    Konyvespolc

---
+++ Osztaly_Konyv/KonyvesPolc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztaly_Konyv
+{
+    public class KonyvesPolc
+    {
+
+        private List<Konyv> _konyvek = new List<Konyv>();
+
+        public KonyvesPolc(List<Konyv> konyvek)
+        {
+            this._konyvek = konyvek;
+        }
+
+        public KonyvesPolc()
+        {
+
+        }
+
+
+        public void addKonyv(Konyv konyv)
+        {
+            _konyvek.Add(konyv);
+        }
+
+        public void removeKonyv(Konyv konyv)
+        {
+            if(_konyvek.Contains(konyv))
+            {
+                _konyvek.Remove(konyv);
+            }
+        }
+
+        public int KonyvekSzama { 
+           get { return _konyvek.Count; }
+        }
+
+
+        public int getKonyvekSzama()
+        {
+            return _konyvek.Count;
+        }
+
+    }
+}
